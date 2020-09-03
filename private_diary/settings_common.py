bb0ed45d15bc346a580722dd5b2eeaaec8c4cdb9
ITK13201 <57588603+ITK13201@users.noreply.github.com>
2020-09-04 01:26:07 +0900

    Change SECRET_KEY

--- private_diary/settings_common.py
+++ private_diary/settings_common.py
@@ -23,8 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's+-j2mtghr%b(iln^d@a4m3h6s1gbf(2_b*%flag+p8@%k%khf'
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 
 # Application definition
